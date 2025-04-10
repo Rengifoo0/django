84f86ddcbc015287c3ba9356c727d7c3d186c185
Rengifoo0 <sebastianrengio0731@gmail.com>
2025-04-10 17:11:27 -0500

    subo trabajo django

--- todo/urls.py
+++ todo/urls.py
@@ -4,5 +4,8 @@ from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("agregar/", views.agregar, name="agregar"),
+    path('', views.home, name='home'),
+    path ("agregar/", views.agregar, name="agregar"),
+    path ("eliminar/<int:tarea_id>/", views.eliminar,name="eliminar"),
+    path ("editar/<int:tarea_id>/", views.editar, name="editar"),
 ]
